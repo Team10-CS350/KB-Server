6087deb6fde30309938a81f81bc2c29751690f3e
jokersus <lolivampireslave@gmail.com>
2019-12-07 18:31:09 +0900

    Added WebMvc config for DTO mapping

---
+++ src/main/java/com/example/demo/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.example.demo.dto.DTOModelMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new DTOModelMapper());
+    }
+}
