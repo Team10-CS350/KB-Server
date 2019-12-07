62310923054e4e5d6bef07422d912bca5a5a1ec6
jokersus <lolivampireslave@gmail.com>
2019-12-07 18:33:59 +0900

    UserDTO has the correct annotations now

--- src/main/java/com/example/demo/dto/UserDTO.java
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -1,6 +1,16 @@
 package com.example.demo.dto;
 
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+import org.springframework.ui.ModelMap;
+
+import javax.validation.constraints.NotNull;
+
+@DTO
+@Data
 public class UserDTO {
+    @NotNull
     private Long id;
+
     private String name;
 }
