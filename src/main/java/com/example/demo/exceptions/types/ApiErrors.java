2844f1bd3be4fc909642494610a9ac5ec41ce33a
jokersus <lolivampireslave@gmail.com>
2019-12-09 00:35:11 +0900

    Switch to custom exception based error-handling

---
+++ src/main/java/com/example/demo/exceptions/types/ApiErrors.java
@@ -0,0 +1,6 @@
+package com.example.demo.exceptions.types;
+
+final class ApiErrors {
+    public static final String USER_NOT_FOUND = "Requested user was not found";
+    public static final String USER_ALREADY_REGISTERED = "A user already exists with given email";
+}
