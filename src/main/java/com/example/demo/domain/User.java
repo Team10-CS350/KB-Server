17d52c7f80476a0f75ee8faaba55817c7e9d747f
jokersus <lolivampireslave@gmail.com>
2019-12-09 00:42:20 +0900

    Validation and relations for future

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -3,15 +3,35 @@ package com.example.demo.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
-@Table(name = "kb_user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String name;
-    private String password = "hmm";
+
+    @NotEmpty
+    @Email()
+    private String email;
+
+//    @Size(min = 8, max = 200)
+    private String password;
+
+//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinTable(
+//            name = "users_roles",
+//            joinColumns = @JoinColumn(
+//                    name = "user_id", referencedColumnName = "id"),
+//            inverseJoinColumns = @JoinColumn(
+//                    name = "role_id", referencedColumnName = "id"))
+//    private Collection< Role > roles;
 
 }
