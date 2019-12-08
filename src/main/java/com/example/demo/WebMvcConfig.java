fc343f07cfc1787eaf0df75864b1c654d75d2af1
jokersus <lolivampireslave@gmail.com>
2019-12-09 00:39:07 +0900

    Typo

--- src/main/java/com/example/demo/WebMvcConfig.java
+++ src/main/java/com/example/demo/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class 1WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
