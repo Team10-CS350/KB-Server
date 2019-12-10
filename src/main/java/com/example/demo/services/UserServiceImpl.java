a4ae52c90f16a336c1c590267a7df87ca78e0dd5
jokersus <lolivampireslave@gmail.com>
2019-12-10 22:39:41 +0900

    Added login function in UserService (unused)

--- src/main/java/com/example/demo/services/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -4,10 +4,16 @@ import com.example.demo.domain.User;
 import com.example.demo.dto.UserRegistrationDTO;
 import com.example.demo.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -40,10 +46,26 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByEmail(email);
     }
 
+//    @Override
+//    public User registerUser(UserRegistrationDTO userRegistrationDTO) {
+//        User newUser = new User();
+//        modelMapper.map(userRegistrationDTO, newUser);
+//        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+//        return saveUser(newUser);
+//    }
+
     @Override
-    public User registerUser(UserRegistrationDTO userRegistrationDTO) {
-        User newUser = new User();
-        modelMapper.map(userRegistrationDTO, newUser);
-        return saveUser(newUser);
+    public String login(String email, String password) {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+
+        if (!userOpt.isPresent()) {
+            return "";
+        }
+
+        String token = UUID.randomUUID().toString();
+        User user = userOpt.get();
+//        user.setToken(token);
+//        customerRepository.save(custom);
+        return token;
     }
 }
