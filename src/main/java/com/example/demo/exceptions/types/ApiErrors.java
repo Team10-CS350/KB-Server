3845c7484e2f4479e908b862e69e24776ec7029c
jokersus <lolivampireslave@gmail.com>
2019-12-11 09:38:06 +0900

    Added new error String constants

--- src/main/java/com/example/demo/exceptions/types/ApiErrors.java
+++ src/main/java/com/example/demo/exceptions/types/ApiErrors.java
@@ -2,5 +2,10 @@ package com.example.demo.exceptions.types;
 
 final class ApiErrors {
     public static final String USER_NOT_FOUND = "Requested user was not found";
+    public static final String RESOURCE_NOT_FOUND = "Requested resource was not found";
     public static final String USER_ALREADY_REGISTERED = "A user already exists with given email";
+    public static final String UNAUTHORIZED = "You do not have sufficient permissions to perform this action";
+    public static final String ALREADY_IN_EVENT = "You are already a member of this event";
+    public static final String NOT_IN_EVENT = "You are not a member of this event";
+
 }
